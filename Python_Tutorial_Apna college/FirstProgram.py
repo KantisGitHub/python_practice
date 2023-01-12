c63e6c9160f21b0e6b014eec35d26aaed2809583
Kanti Pramanik <106877651+kanti-pramanik@users.noreply.github.com>
2023-01-12 11:35:15 +0530

    Add files via upload

---
+++ Python_Tutorial_Apna college/FirstProgram.py
@@ -0,0 +1,122 @@
+# name=("kanti pramanik")
+# age=21
+#
+# print(name,age)
+# print()
+# print(name)
+# print(age)
+
+
+# name=input("what is your name?")
+# print(name)
+#
+# name=input("what is your name?")
+# print("Hello",name)
+
+
+#type converson variable
+# print('''
+# input()
+# flot()
+# eval()
+# bool()
+# ''')
+# old_age=input("Enter the your old age:")
+# new_age=eval(old_age)+2
+# print(new_age)
+
+# first = input("enter the first number:")
+# second = input("enter the second number:")   #method 1
+# sum = int(first)+int(second)
+# print(sum)
+
+            #int
+
+# first = int(input("enter the first number:"))
+# second = int(input("enter the second number:"))   #method 2
+# print(first+second)
+
+            #float
+
+# first = float(input("enter the first number:"))
+# second = float(input("enter the second number:"))   #method 2
+# print(first+second)
+#
+# #etc.
+
+                                                     #string
+
+
+# name="tony stark"
+# print(name.upper())      #all capital
+
+# name="tony stark"
+# print(name.lower())        #all lower
+
+
+#how tofind string
+
+# name="tony stark"
+# print(name.find('s'))  #t=0 o=1 n=2 y=3 space=4 s=5 ||arokom vabe find koreche
+
+#how to replace string
+
+# name="tony stark"
+# print(name.replace("tony stark", "ironman"))
+#
+# print(name.replace("t","m"))
+
+#how to search boolearn in string of python
+# name="tony stark"
+# print("t"in name)   #karon t ta amader string ar modhe ache
+#
+# name="tony stark"
+# print("p"in name)    #karon p ta amader string ar modhe ache
+
+
+
+                                              #Arithmetic Operator
+
+#a khane amra Addition "+", Substract "-", Multipley "*", Divide "/", Modulas "%" asob korte parbo
+
+# rint(5+2)
+# print(5-2)
+# print(5*2)
+# print(5/2)
+# print(5%2)
+# # print(5**2)
+# # print(5**3)
+# print(5**4)p
+
+
+
+                                                #Shortcuts of python
+
+# i=5
+# i=i+2
+# print(i)
+# i+=2
+# print(i)
+# i-=2
+# print(i)
+# i*=2
+# print(i)
+
+
+#operator precedence
+# result=2+3*5
+# print(result)
+# result=7+3*5-2
+# print(result)
+# result=7+3/5-2
+# print(result)
+# result=7+6*3
+# print(result)
+# result=(2+3)*5
+# print(result)
+
+
+                                             #Comparsion Operators
+
+print(3>=2)
+print(1<=2)
